1446146592fedc0f22eae286b679a1a042b95ff2
Paul Beusterien <paulbeusterien@google.com>
2019-07-24 07:57:33 -0700

    3 scripts

--- cirrus.sh
+++ cirrus.sh
@@ -4,4 +4,4 @@ echo "Hello Paul"
 pod --version
 bundle install
 pod --version
-./scripts/pod_lib_lint.rb FirebaseCore.podspec --verbose --platforms=osx
+./scripts/pod_lib_lint.rb FirebaseCore.podspec --verbose --platforms=tvos
